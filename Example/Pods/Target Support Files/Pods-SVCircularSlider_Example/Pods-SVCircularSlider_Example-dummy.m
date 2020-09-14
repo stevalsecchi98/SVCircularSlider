22e483e3e104ca6155ba32f9f8343ee0d500419a
Stefano Valsecchi <stefano.valsecchi@vidiemme.lan>
2020-09-14 17:05:38 +0200

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-SVCircularSlider_Example/Pods-SVCircularSlider_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SVCircularSlider_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SVCircularSlider_Example
+@end
---
+++ Example/Pods/Target Support Files/SVCircularSlider/SVCircularSlider-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SVCircularSlider : NSObject
+@end
+@implementation PodsDummy_SVCircularSlider
+@end
