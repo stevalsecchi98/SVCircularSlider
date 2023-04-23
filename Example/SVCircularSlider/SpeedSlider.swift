aea4a1a660cfb4108995bdc678844f92ec5f9a1c
Ali madhoun <alimadhounvipsa@gmail.com>
2023-04-23 22:54:42 +0200

    - Added onProgressChanged, onToucEnd and onTouchesBegan and track Slider's progress - Support drawing a progress slider with more than two colors gradient - Draw Disk color on progress changed - Added Extra Label into the Slider for better UX - Added a stroke color into the slider's knob - Fix minor issue related with sliding into the max progress

---
+++ Example/SVCircularSlider/SpeedSlider.swift
@@ -0,0 +1,38 @@
+//
+//  SpeedSlider.swift
+//  SVCircularSlider_Example
+//
+//  Created by ali  on 23/04/2023.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import SVCircularSlider
+
+class SpeedSlider: CircularSlider {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        progressColors = [
+            .white.withAlphaComponent(0.1),
+            .white.withAlphaComponent(0.6),
+            .blue.withAlphaComponent(0.9),
+        ]
+        
+        counterColor = .blue
+        diskColor = .blue.withAlphaComponent(0.1)
+        titleFont = .systemFont(ofSize: 14)
+        progressFont = .systemFont(ofSize: 12)
+        titleColor = .black
+        progressColor = .black
+        knobColor = .white
+        backgroundColor = .clear
+        knobBorderColor = .blue
+        knobBorderWidth = 3
+        sliderTitle = "Speed"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
